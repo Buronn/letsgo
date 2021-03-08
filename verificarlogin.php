3e7085cc7b2b992be52825c337fd7dca386ac7f7
Flyder78 <flyderheroesofthestorm@gmail.com>
2021-03-08 09:48:31 -0300

    verificar login redy

--- verificarlogin.php
+++ verificarlogin.php
@@ -1,6 +1,14 @@
 <?php
-    require 'conexion.php';
-    session_start();
-    $usuario = $_POST['user'];
-    $password = $_POST['pass'];
-    $sql = "select Nombre,Password from usuarios"
+require 'conexion.php';
+session_start();
+$usuario = $_POST['user'];
+$password = $_POST['pass'];
+$sql = "select Nombre,Password from usuarios where Nombre='" . $usuario . "' and Password='" . $password . "'";
+$resultado = $conexion->query($sql);
+if (mysqli_num_rows($resultado) == 1) {
+    $_SESSION['login'] = "conexion good";
+    header("Location:home.php");
+} else {
+    $_SESSION['loginf'] = "El usuario o la contraseña son incorrectos";
+    header("Location: index.php");
+}
