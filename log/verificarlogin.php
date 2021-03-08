ed697599dca6cc32731a63a6a978e4537525bcfb
Buronn <Buron@users.noreply.github.com>
2021-03-08 19:28:21 -0300

    push

--- log/verificarlogin.php
+++ log/verificarlogin.php
@@ -7,13 +7,12 @@ $password = $_POST['pass'];
 $sql = "select Nombre,Password from usuarios where Nombre='" . $usuario . "' and Password='" . $password . "'";
 
 $resultado = $conexion->query($sql);
-if (mysqli_num_rows($resultado) == 1) {
 
-    header("Location:../routes/punto.html");
+if (mysqli_num_rows($resultado) == 1) {
+    echo "<script>window.top.location='../routes/punto.html'</script>";
 } else {
     $salida .= "<div class=\"alert alert-warning\" role=\"alert\">
                 Clave o usuario incorrectos
                 </div>";
-    
 }
 echo $salida;
