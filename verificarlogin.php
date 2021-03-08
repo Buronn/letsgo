723fa42b564b032b094261d4939dce143354bbce
Flyder78 <flyderheroesofthestorm@gmail.com>
2021-03-08 17:12:10 -0300

    xd

--- verificarlogin.php
+++ verificarlogin.php
@@ -4,12 +4,15 @@ session_start();
 
 $usuario = $_POST['user'];
 $password = $_POST['pass'];
+echo $usuario;
+echo $password;
 $sql = "select Nombre,Password from usuarios where Nombre='" . $usuario . "' and Password='" . $password . "'";
+echo $sql;
 $resultado = $conexion->query($sql);
 if (mysqli_num_rows($resultado) == 1) {
     $_SESSION['login'] = "conexion good";
     header("Location:home.php");
 } else {
     $_SESSION['loginf'] = "El usuario o la contraseña son incorrectos";
-    header("Location: index.php");
+    header("Location:index.html");
 }
