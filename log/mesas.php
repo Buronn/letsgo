fd94da921084efe879eeb533c67495c5a142b272
Buronn <Buron@users.noreply.github.com>
2021-03-11 01:42:35 -0300

    LETSGO LETSGO LETSGO

--- log/mesas.php
+++ log/mesas.php
@@ -4,23 +4,26 @@ $salida = "";
 $lugar = $_POST['lugar'];
 $ancho = $_POST['ancho'];
 $largo = $_POST['largo'];
-$sql = "Select Mesa,x,y from mesas where Punto='BAR'";
+$sql = "Select Mesa,x,y from mesas where Punto='".$lugar."' and x is not null and y is not null";
 $result = $conexion->query($sql);
 $ancho = (float)$ancho / 10;
 $largo = (float)$largo / 10;
-$salida .= "<div class='sesion'><img src='../images/Fondo/piso.jpg' >";
+$salida .= "<div class='mapa'><img class='img-fluid' usemap='#workmap' src='../images/Fondo/piso2.jpg' >";
+$salida .= "<map name=\"workmap\">";
+
 while ($fila = $result->fetch_assoc()) {
-    $salida .= "<a href='../routes/orden.html'>
-    <img style='position: relative;
-    left: " . $ancho * ((float)$fila['x']) . "px;
-    top: " . $largo * ((float)$fila['y']) . "px;
-    width: 80px;
-    height: 80px;' href='../routes/orden.html' src='../icons/Mesa Redonda 4 Sillas/Mesa Blanco/vacia.gif'>
+    $salida .= "<area src='../icons/Mesa Redonda 4 Sillas/Mesa Blanco/vacia.gif target='_self' alt='algo' title='algo' href='' coords=\"" . $ancho * ((float)$fila['x']+1) . "," . $largo * ((float)$fila['y']+1) . ",44\" shape='circle'>";
+    $salida .= "<script>console.log(" . (((float)$fila['x'])*100)/$ancho . ")</script><a href='../routes/orden.html'>
+    <img class='img-fluid mesitas' style='position: fixed;
+    left: " . ((((float)$fila['x'])*100)/$ancho*6.8) . "%;
+    top: " . (((float)$fila['y'])*110)/$largo*4.5 . "%;
+    width: 9.6%;
+    height: 9.6%;' href='../routes/orden.html' src='../icons/Mesa Redonda 4 Sillas/Mesa Blanco/vacia.gif'>
     </a>
     ";
+    
+    /* coords=\"" . $ancho * ((float)$fila['x']+1) . "," . $largo * ((float)$fila['y']+1) . ",44\" */
 }
+$salida .= "</map>";
 $salida .= "</div>";
-
-
-
 echo "$salida";
