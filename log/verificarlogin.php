4bb15c281fab8e7e40b5613b7b6cfec55925c430
Buronn <Buron@users.noreply.github.com>
2021-03-11 22:07:25 -0300

    owo

--- log/verificarlogin.php
+++ log/verificarlogin.php
@@ -9,13 +9,30 @@ $sql = "select Nombre,Password from usuarios where Nombre='" . $usuario . "' and
 $resultado = $conexion->query($sql);
 
 if (mysqli_num_rows($resultado) == 1) {
-    echo "
+  echo "
+    <script>
+    array = toUTF8Array('" . $usuario . "');
+    var a = '';
+    array.forEach(element => {
+      a = a+element;
+    });
+    createCookie('u_lg',a,0.1);
+    function createCookie(cookieName,cookieValue,daysToExpire)
+        {
+          var date = new Date();
+          date.setTime(date.getTime()+(daysToExpire*24*60*60*1000));
+          document.cookie = cookieName + '=' + cookieValue + '; expires=' + date.toGMTString()+';priority= high';
+        }
+    
+      
+      
+    </script>
   <div class=\"spinner-grow\" style=\"width: 3rem; height: 3rem;\" role=\"status\">
     <span class=\"visually-hidden\"></span>
   </div></span>
     </div><script>setTimeout(() => { window.top.location='../routes/punto.html';},2000);</script>";
 } else {
-    $salida .= "<div class=\"alert alert-warning\" role=\"alert\">
+  $salida .= "<div class=\"alert alert-warning\" role=\"alert\">
                 Clave o usuario incorrectos
                 </div>";
 }
