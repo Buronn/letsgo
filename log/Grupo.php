6c61b255d175a4f61360cfbc26b1b19acd5044e3
Buronn <Buron@users.noreply.github.com>
2021-04-11 17:21:03 -0400

    xd

--- log/Grupo.php
+++ log/Grupo.php
@@ -4,22 +4,22 @@ require "conexion.php";
 $salida = "";
 $sql = "select * from grupos where Clase='" . intval($clase) . "'";
 $resultado = $conexion->query($sql);
-$salida .= "<div class=row>";
+$salida .= "<div class=nav>";
 $aux = 0;
 while ($fila = $resultado->fetch_assoc()) {
 
     $n = $fila['NGrupo'];
     $n = str_replace(" ", "", $n);
-    $salida .="<div class='card' style='width: 18rem;'>
-  <img style='min-height: 30vw;max-height: 30vw'id=$n src='../icons/cargando.gif' class='card-img-top' alt='...'>
-  
-  <div class='card-body'>
-    <a href='#' class='btn btn-primary'>" . $fila['NGrupo'] . "</a>
-  </div>
-</div>
-<script>
-getImage('$n','" . $fila['NGrupo'] . "',$aux);
-  </script>";
+    $salida .="
+    ".
+      /* <img style='min-height: 30vw;max-height: 30vw'id=$n src='../icons/cargando.gif' class='card-img-top' alt='...'> */
+      "<div class='card-body'>
+        <a style='min-height:6vw;min-width:25vw;max-width:25vw;max-height:6vw;font-size:1.5vw' href='#' class='btn btn-dark'>" . $fila['NGrupo'] . "</a>
+      </div>
+   ";
+    /* <script>
+      getImage('$n','" . $fila['NGrupo'] . "',$aux);
+    </script>"; */
   $aux = $aux + 500;
 }
 $salida.="</div>";
--- log/clases.php
+++ log/clases.php
@@ -9,12 +9,12 @@ while ($fila = $resultado->fetch_assoc()) {
     
     if($contador>0){
         $salida .= "<a class='nav-item nav-link' id='nav-clase-tab' data-toggle='tab' href='#nav-'".$fila['Clase']."'' role='tab'
-        aria-controls='nav-home' aria-selected='true' onclick=Grupo('".$fila['Clase']."')>".$fila['NClase']."</a>"
+        aria-controls='nav-home' aria-selected='true' onclick=Grupo('".$fila['Clase']."'),SetLocalStorage('clase','".$fila['Clase']."')>".$fila['NClase']."</a>"
         ;
         
     }else{
         $salida .= "<a class='nav-item nav-link active' id='nav-clase-tab' data-toggle='tab' href='#nav-'".$fila['Clase']."'' role='tab'
-    aria-controls='nav-home' aria-selected='true' onclick=Grupo('".$fila['Clase']."')>".$fila['NClase']."</a>
+    aria-controls='nav-home' aria-selected='true' onclick=Grupo('".$fila['Clase']."'),SetLocalStorage('clase','".$fila['Clase']."')>".$fila['NClase']."</a>
     <script>Grupo('".$fila['Clase']."')</script>";
 
     }
@@ -39,22 +39,5 @@ while($fila = $resultado->fetch_assoc()){
     
 }
 $salida.="</div>";
-/* <div class="tab-content" id="nav-tabContent">
-<div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-    <script>
-        Clases();
-    </script>
-
-    </div>
-</div>
-<div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-    <div class="nav nav-tabs" id="grupos"></div>
-</div>
-<div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-    <input type="text" name="caja_busqued" id="caja_busqueda"></input>
-    <div class="col" id="agregados"></div>
-    <div id="prueba"></div>
-</div>
-</div> */
 
 echo $salida;
