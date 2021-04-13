bcc2c609ae700819e5c000158b63d4fb053aa618
Flyder78 <flyderheroesofthestorm@gmail.com>
2021-04-13 02:20:26 -0400

    diseño orden ahora si xd

--- log/agregarproducto.php
+++ log/agregarproducto.php
@@ -9,18 +9,24 @@ if ($nombre == 'actualizar') {
     $sql = "select NProducto,valor,cantidad from orden where punto='" . $punto . "'and mesa='" . $mesa . "'";
     $resultado = $conexion->query($sql);
     $total = 0;
+    $salida .= "<div>";
     while ($fila = $resultado->fetch_assoc()) {
         $a = $fila['NProducto'];
         $a = str_replace(" ", "&nbsp;", $a);
         $xd = str_replace(" ", "&nbsp;", $fila['NProducto']);
         $a = strtolower($a);
         $a = ucwords($a);
-        $salida .= "
-        <h3 >" . $a . ".</h3>
-        <h3>x" . $fila['cantidad'] . " _____________________ $" . $fila['valor'] . "&ensp;&ensp;&ensp;&ensp;<button class='btn btn-danger' onclick=Borrar('$xd'),Select('actualizar','')>-</button></h3>
-        ";
+        $salida .= "<p class='input-group mb-3'>
+        <span class='btn btn-primary' onclick=Borrar('$xd'),Select('actualizar','')>x" . $fila['cantidad'] . "</span>
+        <a type='text' class='form-control btn-lg'>" . $a . "</a>
+        <span class='btn btn-success' onclick=Borrar('$xd'),Select('actualizar','')>$" . $fila['valor'] . "</span>
+        <span class='btn btn-danger' onclick=Borrar('$xd'),Select('actualizar','')>-</span>
+        <span class='btn btn-danger' onclick=Borrar('$xd'),Select('actualizar','')>Delete</span>
+        <span class='btn btn-danger' onclick=Borrar('$xd'),Select('actualizar','')>note</span>
+        </p>";
         $total = $total + $fila['valor'];
     }
+    $salida .= "</div>";
     $salida .= "<h3 class='titulo'>Total=$$total</h3>";
     echo $salida;
 } else {
@@ -44,10 +50,11 @@ if ($nombre == 'actualizar') {
         $xd = str_replace(" ", "&nbsp;", $fila['NProducto']);
         $a = strtolower($a);
         $a = ucwords($a);
-        $salida .= "
-                <h3 class='word-adjust'>" . $a . ".</h3>
-                <h3>x" . $fila['cantidad'] . " _____________________ $" . $fila['valor'] . "&ensp;&ensp;&ensp;&ensp;<button class='btn btn-danger' onclick=Borrar('$xd'),Select('actualizar','')>-</button></h3>
-                ";
+        $salida .= "<p class='input-group mb-3'>
+        <a type='text' class='form-control'>" . $a . ".x" . $fila['cantidad'] . "$" . $fila['valor'] . ";</a>
+        <span class='btn btn-danger' onclick=Borrar('$xd'),Select('actualizar','')>-</span>
+        <span class='btn btn-danger' onclick=Borrar('$xd'),Select('actualizar','')>Delete</span>
+        </p>";
         $total = $total + $fila['valor'];
     }
     $salida .= "<h3 class='titulo'>Total=$$total</h3>";
--- log/showproduct.php
+++ log/showproduct.php
@@ -7,12 +7,14 @@ $punto = $_POST['punto'];
 $sql="Select p.Producto,p.NProducto,p.Grupo,p.Clase,t.Valor from productos as p INNER JOIN tarifas as t ON p.Clase=t.Clase and p.Grupo=t.Grupo and p.Producto=t.Codigo where  t.Punto='$punto' and p.Baja=0 and p.Grupo='$grupo' and p.Clase='$clase' ;";
 $resultado = $conexion->query($sql);
 while ($fila = $resultado->fetch_assoc()) {
+  $a = $fila['NProducto'];
+  $a = str_replace(" ", "&nbsp;", $a);
     
     $salida .="
     ".
       /* <img style='min-height: 30vw;max-height: 30vw'id=$n src='../icons/cargando.gif' class='card-img-top' alt='...'> */
       "<div class='card-body col-4'>
-        <a style='min-height:6vw;min-width:25vw;max-width:25vw;max-height:7vw;font-size:1.5vw' href='#' class='btn btn-dark'>" . $fila['NProducto'] . "
+        <a style='min-height:6vw;min-width:25vw;max-width:25vw;max-height:7vw;font-size:1.5vw' href='#' class='btn btn-dark' onclick=Select('$a','".$fila['Valor']."')>" . $fila['NProducto'] . "
         $".$fila['Valor']."</a>
       </div>
    ";
