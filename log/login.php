64689908c4edc55624643df68cab7b3f0935c229
Buronn <Buron@users.noreply.github.com>
2021-05-04 20:09:12 -0400

    orden

--- log/login.php
+++ log/login.php
@@ -31,7 +31,7 @@ if (mysqli_num_rows($resultado) == 1) {
   <div class=\"spinner-grow\" style=\"width: 3rem; height: 3rem;\" role=\"status\">
     <span class=\"visually-hidden\"></span>
   </div></span>
-    </div><script>setTimeout(() => { window.top.location='../routes/mapa.html';},2000);</script>";
+    </div><script>setTimeout(() => { window.top.location='../mapa.html';},2000);</script>";
 } else {
   $salida .= "<div class=\"alert alert-warning\" role=\"alert\">
                 Clave o usuario incorrectos
