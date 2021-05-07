4cab7d104c50b2e4584e34673b0a2e6bee2fd832
Buronn <Buron@users.noreply.github.com>
2021-05-07 18:48:38 -0400

    letsgo

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,7 +1,7 @@
 <?php
-define("host_bd", "25.46.196.238");
+define("host_bd", "database-merken.ccvzxrt75imq.us-east-1.rds.amazonaws.com");
 define("user_bd", "root");
-define("pass_bd", "");
+define("pass_bd", "merquen1");
 define("name_bd", "merquen");
 $conexion = new mysqli(
     constant("host_bd"),
